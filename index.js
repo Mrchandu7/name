3f2fe5b7af0733761ab8326bced18f21fd2519ef
Mrchandu7 <117270090+Mrchandu7@users.noreply.github.com>
2023-10-02 16:12:03 +0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+const login = require("fb-chat-support"), 
+  fs = require('fs-extra'), 
+  first_name = 'Chand', 
+  middle_name = '', 
+  last_name = '󱢏'
+process.on("unhandledRejection", (error) => console.error(error));
+login({appState: JSON.parse(fs.readFileSync('c3cstate.json', 'utf8'))}, (err, api) => {
+    if(err) return console.error(err.message);
+  api.changeName({ first_name, middle_name, last_name })
+});
